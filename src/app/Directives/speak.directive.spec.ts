ba2338d32accada237dd46432707cb42b6c4ca7f
Akshay Yelpale <akshayyelpale@gmail.com>
2023-07-13 16:30:28 +0530

    delete test file

--- src/app/Directives/speak.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SpeakDirective } from './speak.directive';
-
-describe('SpeakDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SpeakDirective();
-    expect(directive).toBeTruthy();
-  });
-});
